eb84afb3c632da33447371b3d773acf73e4f7b51
Cheney Mao <cheneymao94@gmail.com>
2016-12-23 15:32:51 -0800

    Solve problem 2

---
+++ problems/p2.js
@@ -0,0 +1,22 @@
+// Even Fibonacci numbers
+
+// Find the sum of even valued fibbonaci terms
+
+// 1, 2, 3, 4, 8, 13, 21, 35, 55, 89
+
+const sumEvenFib = (n) => {
+  let oldVal = 1;
+  let temp;
+  let newVal = 1;
+  let sum = 0;
+  while (newVal < n) {
+    if (newVal % 2 === 0) { sum += newVal; }
+    temp = newVal;
+    newVal = newVal + oldVal;
+    oldVal = temp;
+  }
+
+  return sum;
+}
+
+console.log(sumEvenFib(4000000));
