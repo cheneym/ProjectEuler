7c14168ad2cbf6d5d61a4b0a6315db2cfd9183a7
Cheney Mao <cheneymao94@gmail.com>
2016-12-25 02:25:03 -0800

    Solve problem 10

---
+++ problems/p10.js
@@ -0,0 +1,21 @@
+// Summation of primes
+
+// Find the sum of all the primes below two million.
+
+const getSumPrimesBelow = (n) => {
+  const allNums = [];
+  let sum = 0;  
+  
+  for (let i = 2; i < n; i++) {
+    if (allNums[i] === undefined) {
+      sum += i;
+      for (let j = i * i; j <= n; j += i) {
+        allNums[j] = false;
+      }
+    }
+  }
+
+  return sum;
+};
+
+console.log(getSumPrimesBelow(2000000));
